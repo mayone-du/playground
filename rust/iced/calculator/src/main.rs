1f048833efdef7281a80673ea0aa4ba27c7997f2
mayone-du <cocomayo1201@icloud.com>
2021-12-05 09:16:49 +0900

    wip: calculator

--- rust/iced/calculator/src/main.rs
+++ rust/iced/calculator/src/main.rs
@@ -1,15 +1,20 @@
-use iced::{text_input, Application, Button, Column, Command, Settings, TextInput};
+use iced::{
+    button, text_input, Button, Color, Column, Command, Element, Sandbox, Settings, Text, TextInput,
+};
 
 pub fn main() -> iced::Result {
     State::run(Settings::default())
 }
 
+// アプリケーションのstate
 #[derive(Debug, Default)]
 struct State {
     value: i32,
     current_input: text_input::State,
+    operation_button: button::State,
 }
 
+// 操作
 #[derive(Debug, Clone)]
 enum Operation {
     Add,
@@ -19,16 +24,47 @@ enum Operation {
     Reset,
 }
 
+// メッセージ
 #[derive(Debug, Clone)]
 enum Message {
     ButtonPressed(Operation),
     CurrentInputChanged(i32),
 }
 
-impl Application for State {
+impl Sandbox for State {
     type Message = Message;
 
-    fn new(_flags: ()) -> (Command<Message>) {
-        (Command::perform({}, Message::ButtonPressed(Operation::Reset)))
+    fn new() -> Self {
+        Self::default()
+    }
+
+    fn title(&self) -> String {
+        String::from("Calculator")
+    }
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::ButtonPressed(operation) => match operation {
+                Operation::Add => {
+                    println!("add");
+                }
+                _ => {
+                    println!("other");
+                }
+            },
+            _ => {}
+        }
+    }
+
+    fn view(&mut self) -> Element<Message> {
+        // カラム
+        Column::new()
+            .padding(20)
+            // ボタンを生成し、クリックイベントにMessageのIncrementPressedを指定
+            .push(Button::new(
+                &mut self.operation_button,
+                Text::new("add").color(Color::from_rgb(100.0, 100.0, 0.0)),
+            ))
+            .into()
     }
 }
