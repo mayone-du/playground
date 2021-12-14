984278ee8c8b532a3588e6fee843a2ae9b47357f
mayone-du <cocomayo1201@icloud.com>
2021-12-14 23:06:30 +0900

    add: sample codes

--- rust/cli/nextjs-routes-type-gen/src/main.rs
+++ rust/cli/nextjs-routes-type-gen/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::{fs, io};
+
+fn main() -> io::Result<()> {
+    let mut entries = fs::read_dir("./example/src/pages")?
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()?;
+
+    entries.sort();
+    println!("{:#?}", entries);
+
+    // The entries have now been sorted by their path.
+
+    Ok(())
 }
