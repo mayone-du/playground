bb2ef61b44f389a15d31094df7d00f1020f78291
mayone-du <cocomayo1201@icloud.com>
2022-02-14 22:31:52 +0900

    add: error codes

--- report/next-ts-graphql-codegen-rerender/src/pages/index.tsx
+++ report/next-ts-graphql-codegen-rerender/src/pages/index.tsx
@@ -1,15 +1,22 @@
-import dayjs from "dayjs";
-import { useNewsListQuery } from "../graphql/schemas/generated/schema";
+// import dayjs from "dayjs";
+import {
+  useNewsListQuery,
+  useTestQuery,
+} from "../graphql/schemas/generated/schema";
 
 const IndexPage = () => {
-  const { data, loading, error } = useNewsListQuery({
-    variables: {
-      input: { sharedAt: dayjs() },
-    },
+  const { data, loading, error } = useTestQuery({
+    variables: { customArg: new Date() },
   });
+  // const { data, loading, error } = useNewsListQuery({
+  //   variables: {
+  //     input: { sharedAt: new Date() },
+  //   },
+  // });
   console.log("hoge", data, loading, error);
   return (
     <div>
+      {data?.test}
       {/* {data?.newsList.map((news) => (
         <div key={news.nodeId}>{news.title}</div>
       ))} */}
