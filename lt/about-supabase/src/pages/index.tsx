9008c32eb3bc3913282a91631ef556f0d5aae1a6
Yuki Ishikawa <>
2022-07-27 11:46:24 +0900

    💄

--- lt/about-supabase/src/pages/index.tsx
+++ lt/about-supabase/src/pages/index.tsx
@@ -1,7 +1,25 @@
 import type { NextPage } from "next";
+import { useState } from "react";
 
 const Home: NextPage = () => {
-  return <div></div>;
+  const [title, setTitle] = useState("");
+  return (
+    <main>
+      <h1>ToDoアプリで学ぶSupabase</h1>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+        }}
+      >
+        <input
+          type="text"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <button type="submit">作成</button>
+      </form>
+    </main>
+  );
 };
 
 export default Home;
